9063147b6acd88a707b0f615dd522d41a2160250
Esteban Parra <tevyedu@gmail.com>
2026-07-06 14:53:27 -0400

    Add Bootstrap CSS link to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import 'bootstrap/js/index.esm.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <App />
   </StrictMode>,
 )
